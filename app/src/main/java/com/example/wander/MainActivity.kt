dba21c8b2ec5dfde486ece5214069453a4703e3d
Ifeanyi Mbanefo <ifeanyimbanefo@gmail.com>
2023-09-21 21:12:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wander/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.wander
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.appbar.MaterialToolbar
+import java.util.Locale
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var map : GoogleMap
+
+    private val REQUEST_LOCATION_PERMISSION = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        // OnMapReadyCallback impl
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            val location = LatLng(37.59, -118.86 )
+            val zoomLevel = 10f
+            map = it
+            map.addMarker(MarkerOptions().position(location).title("Marker"))
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location,zoomLevel))
+            setMapLongClick(map)
+            setPOIClick(map)
+            //enableMyLocation()
+        })
+
+        // Map types
+        toolbar.setOnMenuItemClickListener{
+            when (it.itemId){
+                R.id.normal_map -> {
+                    map.mapType = GoogleMap.MAP_TYPE_NORMAL
+                    true
+                }
+                R.id.hybrid_map -> {
+                    map.mapType = GoogleMap.MAP_TYPE_HYBRID
+                    true
+                }
+                R.id.satellite_map -> {
+                    map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+                    true
+                }
+                R.id.terrain_map -> {
+                    map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+                    true
+                }
+                else -> true
+            }
+        }
+
+    }
+
+    private fun setMapLongClick(map: GoogleMap){
+        map.setOnMapLongClickListener {latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            map.addMarker(MarkerOptions().position(latLng).title(getString(R.string.dropped_pin)).snippet(snippet))
+        }
+    }
+
+    private fun setPOIClick(map: GoogleMap){
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker!!.showInfoWindow()
+        }
+    }
+
+    private fun enableMyLocation() {
+        // Check if location permission was granted first
+        // before enabling my location else request permission first
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+}
